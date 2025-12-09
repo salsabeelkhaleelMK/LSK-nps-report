cec56a9e0c8a45fcab481407fabbbe3d6ce59efb
SalsabeelKhaleelMK <salsabeel.khaleel@Motork.io>
2025-12-09 20:40:15 +0100

    updates

---
+++ .next/dev/static/chunks/LSK-nps-report_app_layout_tsx_d6420e48._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__d9fbd7cb._.css",
+  "static/chunks/97cd1__pnpm_459cbac6._.js"
+],
+    source: "dynamic"
+});
